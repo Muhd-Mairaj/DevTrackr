333fdbe030f31e50c5522190f6a58d7d0182dbcb
Feras Hamid <ferashamidbanday22@gmail.com>
2026-02-27 02:26:42 +0530

    rename user table; timezone aware timestamps

--- backend/app/models/project.py
+++ backend/app/models/project.py
@@ -5,4 +5,4 @@ from .base import BaseModel
 class Project(BaseModel, table=True):
     name: str = Field(index=True)
     description: str | None = Field(default=None)
-    user_id: uuid.UUID = Field(foreign_key="user.id", nullable=False, ondelete="CASCADE")
+    user_id: uuid.UUID = Field(foreign_key="users.id", nullable=False, ondelete="CASCADE")
--- backend/app/models/time_entry.py
+++ backend/app/models/time_entry.py
@@ -1,5 +1,5 @@
 import uuid
-from sqlmodel import Field
+from sqlmodel import Field, DateTime
 from .base import BaseModel
 from datetime import datetime
 
@@ -7,7 +7,13 @@ class TimeEntry(BaseModel, table=True):
     __tablename__ = "time_entry"
     
     description: str | None = Field(default=None)
-    start_time: datetime = Field(nullable=False)
-    end_time: datetime | None = None
+    start_time: datetime = Field(
+        sa_type=DateTime(timezone=True),
+        nullable=False
+    )
+    end_time: datetime | None = Field(
+        default=None,
+        sa_type=DateTime(timezone=True)
+    )
     duration_seconds: int | None = None
     project_id: uuid.UUID = Field(foreign_key="project.id", nullable=False, ondelete="CASCADE")
