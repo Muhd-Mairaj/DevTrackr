fbf29c31856b79453aae5ab9e543ea54be77ccc8
Mairaj <muhd.mairaj2003@gmail.com>
2026-02-26 18:08:00 +0300

    chore: add __all__ declaration in models __init__.py (removes unused variable warning

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -6,3 +6,14 @@ from .project import Project
 from .repository import Repository
 from .time_entry import TimeEntry
 from .user import User
+
+__all__ = [
+    "BaseModel",
+    "Commit",
+    "Integration",
+    "Logbook",
+    "Project",
+    "Repository",
+    "TimeEntry",
+    "User",
+]
