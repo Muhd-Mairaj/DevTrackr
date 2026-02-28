ef984154ae4cbc3582a83e3b41b4b682b64b9a31
Mairaj <muhd.mairaj2003@gmail.com>
2026-02-28 20:12:57 +0300

    refactor: update environment settings to use 'development' instead of 'local'. remove old backend .env.template

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -14,7 +14,7 @@ class Settings(BaseSettings):
 
     PROJECT_NAME: str
     API_V1_STR: str = "/api/v1"
-    ENVIRONMENT: Literal["local", "staging", "production"]
+    ENVIRONMENT: Literal["development", "staging", "production"]
 
     # Security
     SECRET_KEY: str
